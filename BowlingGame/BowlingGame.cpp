7ac1310fd1c74b2c1b2027e788961981a0795390
Youngjoo <my.cat.liang@gmail.com>
2024-05-17 09:37:07 +0900

    initial commit

---
+++ BowlingGame/BowlingGame.cpp
@@ -0,0 +1,17 @@
+class BowlingGame
+{
+public:
+	BowlingGame();
+	~BowlingGame();
+
+private:
+
+};
+
+BowlingGame::BowlingGame()
+{
+}
+
+BowlingGame::~BowlingGame()
+{
+}
---
+++ Test/BowlingGameTest.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+
+TEST(BowlingGameTest, ZeroPointGame) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
